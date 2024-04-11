2e1dde70224f741774c34906d96b015bdb7a9b30
kyle <yun3.jo@gmail.com>
2024-04-11 11:03:30 +0900

    squash merge test

--- ctrl.py
+++ ctrl.py
@@ -20,3 +20,15 @@ class Control:
         
     def sum(self, a, b):
             return a+b
+
+    def sub(self, a, b):
+        return a-b
+
+    def mul(self, a, b):
+        return a*b
+
+    def div(self, a, b):
+        return a/b
+
+    def pow(self, a, b):
+        return pow(a, b)
