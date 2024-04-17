c55c3a512d3664bfc9b5df27f4bc6f3da0c8559e
kyle <yun3.jo@gmail.com>
2024-04-17 17:23:19 +0900

    Modify ui.py to add % operator in Qcombobox

--- ui.py
+++ ui.py
@@ -27,7 +27,7 @@ class View(QWidget):
         self.le2.setAlignment(QtCore.Qt.AlignRight)
         
         self.cb = QComboBox(self)
-        self.cb.addItems(['+', '-', '*', '/', '^'])
+        self.cb.addItems(['+', '-', '*', '/', '^', '%'])
         
         hbox_formular = QHBoxLayout()
         hbox_formular.addWidget(self.le1)
